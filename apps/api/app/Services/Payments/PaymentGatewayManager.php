5a253c25f1db6d77d22209bb02f35fb3d69f6bd7
abramz-mrn <abram.dwiwahyudi@gmail.com>
2026-01-11 09:36:37 +0700

    Add PaymentGatewayManager with webhook handlers

---
+++ apps/api/app/Services/Payments/PaymentGatewayManager.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Services\Payments;
+
+class PaymentGatewayManager
+{
+    public function handleMidtransWebhook(array $data): void {}
+    public function handleXenditWebhook(array $data): void {}
+}
