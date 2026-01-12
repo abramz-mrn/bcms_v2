1c72fdb6c9eb870712b9156a7d7ed6144e354349
abramz-mrn <abram.dwiwahyudi@gmail.com>
2026-01-12 15:08:36 +0700

    Delete apps directory

--- apps/api/app/Services/Payments/PaymentGatewayManager.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Services\Payments;
-
-class PaymentGatewayManager
-{
-    public function handleMidtransWebhook(array $data): void {}
-    public function handleXenditWebhook(array $data): void {}
-}
