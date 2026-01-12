61912b6d94a853c07af92efeab8b4f520940e48a
abramz-mrn <abram.dwiwahyudi@gmail.com>
2026-01-12 15:08:16 +0700

    Delete scripts directory

--- scripts/dev.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -e
-docker compose -f infra/docker/compose/docker-compose.yml -f infra/docker/compose/docker-compose.dev.yml up -d
--- scripts/prod.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-set -e
-docker compose -f infra/docker/compose/docker-compose.yml -f infra/docker/compose/docker-compose.prod.yml up -d
